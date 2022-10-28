e99799e973f4bf8b7698d3d960f7e2c7893e007a
Karen's PC <silence@Karen-PC.localdomain>
2022-10-28 14:14:26 +0400

    sqrt + tests

--- testbed/src/math/test_sqrt.c
+++ testbed/src/math/test_sqrt.c
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   test_sqrt.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/27 23:14:30 by kdaniely          #+#    #+#             */
-/*   Updated: 2022/10/28 13:48:49 by kdaniely         ###   ########.fr       */
+/*   Created: 2022/10/28 12:35:35 by kdaniely          #+#    #+#             */
+/*   Updated: 2022/10/28 14:13:21 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_sqrt(int nb)
-{
-	int i;
-	int j;
-	long curr;
+#include "test_math.h"
 
-	i = 0;
-	j = nb;
-	curr = nb / 2;
-	while (i <= j)
-	{
-		if (curr * curr > (long)nb)
-			j = curr - 1;
-		else if (curr * curr < (long)nb)
-			i = curr + 1;
-		else
-			return (curr);
-		curr = i + (j - i) / 2;
-	}
-	return (0);
+void	test_sqrt(void)
+{
+	printf("sqrt(%d): %d\n", 4, ft_sqrt(4));
+	printf("sqrt(%d): %d\n", 7, ft_sqrt(7));
+	printf("sqrt(%d): %d\n", 81, ft_sqrt(81));
+	printf("sqrt(%d): %d\n", 1073741824, ft_sqrt(1073741824));
+	printf("sqrt(%d): %d\n", 9, ft_sqrt(9));
+	printf("sqrt(%d): %d\n", -34, ft_sqrt(-34));
 }
