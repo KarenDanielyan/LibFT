10b503991734f390b0455aed0531c50d0a85bb22
Karen's PC <silence@Karen-PC.localdomain>
2022-11-08 17:00:17 +0400

    test_strdup

--- testbed/src/str/test_strdup.c
+++ testbed/src/str/test_strdup.c
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_str.h                                         :+:      :+:    :+:   */
+/*   test_strdup.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/29 15:32:58 by kdaniely          #+#    #+#             */
-/*   Updated: 2022/11/08 16:53:02 by kdaniely         ###   ########.fr       */
+/*   Created: 2022/11/08 16:50:09 by kdaniely          #+#    #+#             */
+/*   Updated: 2022/11/08 16:54:40 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TEST_STR_H
-# define TEST_STR_H
-# include <stdio.h>
-# include <libft.h>
-# include <bsd/string.h>
+#include "test_str.h"
 
-void	test_strlen(void);
-void	test_strcpy(void);
-void	test_strncpy(void);
-void	test_strlcpy(void);
-void	test_strcat(void);
-void	test_strncat(void);
-void	test_strlcat(void);
-void	test_strcmp(void);
-void	test_strncmp(void);
-void	test_strnrcmp(void);
-void	test_strrev(void);
-void	test_strdup(void);
-#endif
+static void	helper(void)
+{
+	char	*str;
+
+	str = ft_strdup("If you see this text, then ft_strdup function works!\n");
+	printf("Testing strdup:\n%s", str);
+}
+
+void	test_strdup(void)
+{
+	helper();
+}
