bba5435050c6b8c094251092d896448e12b7db9d
Karen's PC <silence@Karen-PC.localdomain>
2023-01-13 20:55:13 +0400

    Init

---
+++ test/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Build script for testbed
+set echo on
+
+mkdir -p ../bin
+
+# Get a list of all the .c files.
+cFilenames=$(find . -type f -name "*.c")
+
+# echo "Files:" $cFilenames
+
+assembly="testbed"
+compilerFlags="-g -fPIC -Wall -Wextra -Werror -Wpedantic" 
+# -Wall -Werror
+includeFlags="-I./ -I../libft/src/ -I../libft/"
+linkerFlags="-L../bin/ -l_libft -lbsd -Wl,-rpath,."
+
+echo "Building $assembly..."
+echo gcc $cFilenames $compilerFlags -o ../bin/$assembly $includeFlags $linkerFlags
+gcc $cFilenames $compilerFlags -o ../bin/$assembly $defines $includeFlags $linkerFlags
